0683b1a7d34b8710a89d7c1cc7a2af13da7232c1
Mariana Pereira <marianna.22sp@gmail.com>
2020-08-17 10:07:11 -0300

    Changes in header design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 const Header: React.FC = () => (
   <Container>
     <Icon name="explore" size={30} color="#fff" />
+    <Title>GitExplorer</Title>
   </Container>
 );
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,6 +3,13 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   height: 70px;
   background: #28a745;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  margin-left: 5px;
 `;
