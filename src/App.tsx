16976a2aa408222f39ea027032c347d97dcd6a2a
Mariana Pereira <marianna.22sp@gmail.com>
2020-08-18 13:53:14 -0300

    Add theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
+
+import usePersistedState from './utils/usePersistedState';
 
 import store from './store';
 
 import Routes from './routes';
 import Header from './components/Header';
 
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
 const App: React.FC = () => {
+  const [theme, setTheme] = usePersistedState('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar barStyle="light-content" backgroundColor="#28a745" />
-        <Header />
-        <Routes />
-      </NavigationContainer>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <StatusBar barStyle="light-content" backgroundColor="#28a745" />
+          <Header toggleTheme={toggleTheme} />
+          <Routes />
+        </NavigationContainer>
+      </Provider>
+    </ThemeProvider>
   );
 };
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,14 +2,25 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   height: 70px;
-  background: #28a745;
+  background: ${(props) => props.theme.colors.primary};
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  padding: 5px;
 `;
 
 export const Title = styled.Text`
   font-size: 20px;
-  color: #fff;
+  color: ${(props) => props.theme.colors.buttonText};
   margin-left: 5px;
+  font-family: ${(props) => props.theme.fonts.regular};
+`;
+
+export const Logo = styled.View`
+  flex-direction: row;
+`;
+
+export const Button = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
 `;
