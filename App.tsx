e1477c1b60dbee3dd24659158042b7926fffcd74
Mariana <mariprr22@gmail.com>
2024-02-09 13:00:54 -0300

    feat: configure app navigation

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import { Roboto_400Regular, Roboto_700Bold,useFonts } from '@expo-google-fonts/roboto';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { ActivityIndicator, StatusBar } from 'react-native';
 import { Provider as ReduxProvider } from 'react-redux';
 
-import Main from './src/pages/main';
+import { AppRoutes } from './src/routes';
 import { store } from './src/store';
 
 export default function App() {
-	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-	return (
-		<ReduxProvider store={store}>
-			<StatusBar
-				barStyle="light-content"
-				backgroundColor="transparent"
-				translucent
-			/>
-			{fontsLoaded ? <Main /> : <ActivityIndicator /> }
-		</ReduxProvider>
-	);
+  return (
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <AppRoutes /> : <ActivityIndicator /> }
+      </NavigationContainer>
+    </ReduxProvider>
+  );
 }
