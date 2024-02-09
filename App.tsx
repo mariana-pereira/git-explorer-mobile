e8ea6f59af552993986d2b865012699d69db2980
Mariana <mariprr22@gmail.com>
2024-02-09 12:31:35 -0300

    feat: start styling

--- App.tsx
+++ App.tsx
@@ -1,24 +1,22 @@
-import { Provider as ReduxProvider } from 'react-redux';
+import { Roboto_400Regular, Roboto_700Bold,useFonts } from '@expo-google-fonts/roboto';
 import React from 'react';
-import { ActivityIndicator, StatusBar, Text, View } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
+import { Provider as ReduxProvider } from 'react-redux';
+
+import Main from './src/pages/main';
 import { store } from './src/store';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  return (
-    <ReduxProvider store={store}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? (
-        <View>
-          <Text>Open up App.tsx to start working on your app!</Text>
-        </View>
-      ) : <ActivityIndicator /> }
-    </ReduxProvider>
-  );
+	return (
+		<ReduxProvider store={store}>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			{fontsLoaded ? <Main /> : <ActivityIndicator /> }
+		</ReduxProvider>
+	);
 }
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from './styles';
+
+export default function Main() {
+	return (
+		<Container>
+			<Title>Main</Title>
+		</Container>
+	);
+}
