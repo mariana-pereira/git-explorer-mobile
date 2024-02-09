082b50953f31dd968b698a04b20455c87d003db5
Mariana <mariprr22@gmail.com>
2024-02-09 11:45:42 -0300

    feat: edit status bar

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { Provider as ReduxProvider } from 'react-redux';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { store } from './src/store';
 
 export default function App() {
   return (
     <ReduxProvider store={store}>
       <View>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
         <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
       </View>
     </ReduxProvider>
   );
