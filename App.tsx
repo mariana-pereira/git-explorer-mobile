829c0388a9537a8df6a26437f1b669211c3e47f0
Mariana <mariprr22@gmail.com>
2024-02-09 12:07:02 -0300

    feat: personalize fonts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 import { Provider as ReduxProvider } from 'react-redux';
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { ActivityIndicator, StatusBar, Text, View } from 'react-native';
 import { store } from './src/store';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ReduxProvider store={store}>
-      <View>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </View>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <View>
+          <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+      ) : <ActivityIndicator /> }
     </ReduxProvider>
   );
 }
