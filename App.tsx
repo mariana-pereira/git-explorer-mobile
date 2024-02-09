b7a8e47fa269462ac6a6daa44fb670a820f3f08e
Mariana <mariprr22@gmail.com>
2024-02-09 11:32:25 -0300

    feat: configure redux

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import { Provider as ReduxProvider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Text, View } from 'react-native';
+import { store } from './src/store';
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ReduxProvider store={store}>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ReduxProvider>
   );
 }
