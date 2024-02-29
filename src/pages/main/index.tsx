1646712c83b892bd17010a944b948151786f7b3a
Mariana <mariprr22@gmail.com>
2024-02-29 12:18:07 -0300

    feat: add user component

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,15 +1,20 @@
+import { View } from 'react-native';
+
 import { Header } from '../../components/Header';
 import { RepositoryList } from '../../components/RepositoryList';
+import User from '../../components/User';
 import { Container, Content } from './styles';
 
 export default function Main() {
   return (
-    <Container>
-      <Header />
-
-      <Content>
-        <RepositoryList />
-      </Content>
-    </Container>
+    <View style={{ flex: 1 }}>
+      <Container>
+        <Header />
+        <Content>
+          <User />
+          <RepositoryList />
+        </Content>
+      </Container>
+    </View>
   );
 }
